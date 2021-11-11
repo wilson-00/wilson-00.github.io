a05f1ced7903ff3acae5cdfff76a429128277f75
Wilson Chong <wilsonchongrs@gmail.com>
2021-11-11 17:44:07 +0800

    adding structure and js

---
+++ skiing_plan/js/book.js
@@ -0,0 +1,55 @@
+let enterNowBtn = document.getElementById("enterNowBtn")
+enterNowBtn.addEventListener("click", function () {
+    let nameCountry = document.getElementById("nameCountry")
+    let nameCountryVal = nameCountry.value
+
+    let nameLocation = document.getElementById("nameLocation")
+    let nameLocationVal = nameLocation.value
+
+    let nameHotel = document.getElementById("nameHotel")
+    let nameHotelVal = nameHotel.value
+
+    let hotelPriceVal = document.getElementById("hotelPrice").value
+
+    let skiPriceVal = document.getElementById("skiPrice").value
+
+    let skiLessonsVal = document.getElementById("skiLessons").value
+
+    let travelFareVal = document.getElementById("travelFare").value
+
+    let webSiteVal = document.getElementById("webSite").value
+
+
+
+    EnterNow(nameCountryVal, nameLocationVal, nameHotelVal, hotelPriceVal, skiPriceVal, skiLessonsVal, travelFareVal, webSiteVal)
+
+
+
+})
+
+function EnterNow(nameCountryVal, nameLocationVal, nameHotelVal, hotelPriceVal, skiPriceVal, skiLessonsVal, travelFareVal, webSiteVal) {
+    let url = 'https://api.sheety.co/e3348f6ac891bef5d9ac63d9e0945612/skiingInformation/sheet1';
+    let body = {
+        booking: {
+            country: nameCountry,
+            location: nameLocation,
+            hotel: nameHotel,
+            hotelprice: hotelPrice,
+            skiprice: skiPrice,
+            skilessons: skiLessons,
+            travelfare: travelFareVal,
+            website: webSite
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert("Information has been successfully added!")
+        });
+}
---
+++ skiing_plan/js/list.js
@@ -0,0 +1,67 @@
+let refreshNow = document.getElementById("refreshNowBtn")
+refreshNow.addEventListener("click", function () {
+    GetBooking()
+})
+
+function GetBooking() {
+    let url = 'https://api.sheety.co/e3348f6ac891bef5d9ac63d9e0945612/skiingInformation/sheet1';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            let skiingInformationList = document.getElementById("skiingInformationList")
+            let skiingInformationIds = []
+
+            //clear the table/list
+            for (let k = skiingInformationList.rows.length - 1; k > 0; k--) {
+                skiingInformationList.deleteRow(k)
+            }
+
+            //load the new data/list
+            for (let i = 0; i < json.bookings.length; i++) {
+                let gCountry = json.bookings[i].country
+                let gLocation = json.bookings[i].location
+                let gHotel = json.bookings[i].hotel
+                let gHotelPrice = json.bookings[i].hotelprice
+                let gSkiPass = json.bookings[i].skipass
+                let gSkiLessons = json.bookings[i].skilessons
+                let gTravelFare = json.bookings[i].travelfare
+                let gWebsite = json.bookings[i].website
+                let btnId = "delete" + gId
+
+                let row = skiingInformationList.insertRow(skiingInformationList.rows.length)
+                row.insertCell(0).innerHTML = gCountry
+                row.insertCell(1).innerHTML = gLocation
+                row.insertCell(2).innerHTML = gHotel
+                row.insertCell(3).innerHTML = gHotelPrice
+                row.insertCell(4).innerHTML = gSkiPass
+                row.insertCell(5).innerHTML = gSkiLessons
+                row.insertCell(6).innerHTML = gTravelFare
+                row.insertCell(7).innerHTML = gWebsite
+                row.insertCell(8).innerHTML = "<button id='" + btnId + "'  class='btn btn-danger'>Delete</button>"
+
+                skiingInformationIds.push(btnId)
+
+            }
+
+            //attach listener to buttons
+            for (let j = 0; j < skiingInformationIds.length; j++) {
+                let el = document.getElementById(skiingInformationIds[j])
+                el.addEventListener("click", function () {
+                    let theId = skiingInformationIds[j].replace("delete", "")
+                    DeleteBooking(theId)
+                })
+            }
+
+        });
+}
+
+function DeleteBooking(id) {
+    let url = 'https://api.sheety.co/e3348f6ac891bef5d9ac63d9e0945612/skiingInformation/sheet1' + id;
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then(() => {
+            alert("Record id " + id + " deleted!")
+            GetBooking()
+        });
+}
